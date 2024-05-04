6189f47939225f2a0d5193abf2570cd8009dbfa1
shu <chukichi915@gmail.com>
2024-05-04 11:02:59 +0900

    個別ページの関連アイテムにたいするN+1対策をしました

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @related_products = Product.order(id: :desc).limit(4)
+    @related_products = Product.with_attached_image.order(id: :desc).limit(4)
   end
 end
