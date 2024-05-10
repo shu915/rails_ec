4d2c4e692fa92d767f978a9a78c81471e9b6dd2e
shu <chukichi915@gmail.com>
2024-05-10 13:09:18 +0900

    rubocopとフォーマッターを通した

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,7 +15,7 @@ class CartsController < ApplicationController
 
   def create
     @cart_product = current_cart.cart_products.find_or_initialize_by(cart_id: session[:cart_id],
-                                                      product_id: cart_product_params[:product_id])
+                                                                     product_id: cart_product_params[:product_id])
     @cart_product.quantity += cart_product_params[:quantity].to_i
     if @cart_product.save
       flash[:notice] = "#{@cart_product.product.name}が追加されました。"
