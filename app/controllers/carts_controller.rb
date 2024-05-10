7edf55fc7b98dfe7dbfaa24bc18e702a1a5e35b6
shu <chukichi915@gmail.com>
2024-05-10 13:06:54 +0900

    引数として渡すより、アソシエーションで書いたほうがいいと教わりました。またページネーションつけました

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,7 +14,7 @@ class CartsController < ApplicationController
   end
 
   def create
-    @cart_product = CartProduct.find_or_initialize_by(cart_id: session[:cart_id],
+    @cart_product = current_cart.cart_products.find_or_initialize_by(cart_id: session[:cart_id],
                                                       product_id: cart_product_params[:product_id])
     @cart_product.quantity += cart_product_params[:quantity].to_i
     if @cart_product.save
